9f299f3f5bcdb6bfd3952c18da2790a7213095f3
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-12-12 18:57:34 +0600

    InfoCard design complete.

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/components/InfoCard.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/components/InfoCard.kt
@@ -1,14 +1,25 @@
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -25,7 +36,8 @@ fun InfoCard(
     formattedTextStyle: TextStyle = LocalTextStyle.current.copy(
         textAlign = TextAlign.Center,
         fontSize = 18.sp,
-    )
+    ),
+    contentColor: Color = MaterialTheme.colorScheme.onSurface
 ) {
     Card(
         modifier = modifier
@@ -33,10 +45,57 @@ fun InfoCard(
             .shadow(
                 elevation = 15.dp,
                 shape = RectangleShape,
+                ambientColor = MaterialTheme.colorScheme.primary,
                 spotColor = MaterialTheme.colorScheme.primary
-            )
+            ),
+        shape = RectangleShape,
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.primary
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+            contentColor = contentColor
+        )
     ) {
+        AnimatedContent(
+            targetState = icon,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            label = "IconAnimation"
+        ) {icon ->
+            Icon(
+            imageVector = icon,
+            contentDescription = title,
+            modifier = Modifier
+                .size(75.dp)
+                .padding(top = 16.dp)
 
+        ) }
+        Spacer(modifier = Modifier.height(8.dp))
+
+        AnimatedContent(
+            targetState = formattedText,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            label = "ValueAnimation"
+        ) {formattedText ->
+            Text(
+                text = formattedText,
+                style = formattedTextStyle,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            ) }
+        Text(
+            text = title,
+            style = formattedTextStyle,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .align(Alignment.CenterHorizontally),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Light,
+            color = contentColor
+        )
     }
 }
 
